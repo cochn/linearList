9e2d14fc9b8005e641fd20167e48a8a148c4c4e5
曹臣 <caochen@caochendeiMac.local>
2017-07-27 08:47:11 +0800

    Initial Commit

---
+++ LinearList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinearList
+//
+//  Created by 曹臣 on 2017/7/27.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
