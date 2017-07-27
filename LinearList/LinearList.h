caa2787867e23976b5359c9ac16cc0c3f30e2cdf
曹臣 <caochen@caochendeiMac.local>
2017-07-27 16:25:54 +0800

    linearList

---
+++ LinearList/LinearList.h
@@ -0,0 +1,73 @@
+//
+//  LinearList.h
+//  LinearList
+//
+//  Created by 曹臣 on 2017/7/27.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#ifndef LinearList_h
+#define LinearList_h
+
+#include <stdio.h>
+
+#endif /* LinearList_h */
+
+typedef int LinearListNodeValue;
+
+typedef struct {
+    
+    int capacity;
+    int length;
+    LinearListNodeValue * value;
+    
+}LinearList;
+
+/**创建线性表*///用不用指针的话，每次copy的都是深拷贝，浪费内存。使用指针，可以做到浅copy
+LinearList * linearCreat(int capacity);
+
+/**释放内存*/
+void linearFree(LinearList * list);
+
+/**线性表长度*/
+int linearLength(LinearList *list);
+
+/**查询线性表index处的值*/
+LinearListNodeValue linearNodeValueAtIndex(LinearList *list, int index);
+LinearListNodeValue linearFirstValue(LinearList *list);
+LinearListNodeValue linearLastValue(LinearList *list);
+
+/**在index插入value*/
+void linearInsertAtIndex(LinearList *list, int index, LinearListNodeValue value);
+
+/**在末尾添加value*/
+void linearAddValue(LinearList *list, LinearListNodeValue value);
+
+/**删除index处的值*/
+void linearRemoveAtIndex(LinearList *list, int index);
+
+/**删除所有的值*/
+void linearRemoveAll(LinearList *list);
+
+/**删除value*/
+void linearRemoveValue(LinearList *list, LinearListNodeValue value);
+
+/**修改index处的值*/
+void linearUpdateAtIndex(LinearList *list,int index, LinearListNodeValue value);
+
+/**打印*/
+void linearPrint(LinearList *list);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- LinearList/main.m
+++ LinearList/main.m
@@ -7,11 +7,57 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LinearList.h"
+
+void test(){
+    
+    LinearList *list = linearCreat(10);
+    
+    linearAddValue(list, 10);
+    linearAddValue(list, 15);
+    linearAddValue(list, 10);
+    linearAddValue(list, 15);
+    linearAddValue(list, 15);
+    linearAddValue(list, 30);
+    linearAddValue(list, 20);
+    linearAddValue(list, 15);
+    linearInsertAtIndex(list, 0, 10);
+    
+    linearPrint(list);
+    
+    int a = linearLength(list);
+    printf("\nlength=%d\n",a);
+    
+    LinearListNodeValue value = linearNodeValueAtIndex(list, 2);
+    printf("\nvalue=%d\n",value);
+    
+    LinearListNodeValue firstValue = linearFirstValue(list);
+    LinearListNodeValue lastValue = linearLastValue(list);
+    printf("firstValue=%d\nlastValue=%d\n",firstValue,lastValue);
+    
+    linearUpdateAtIndex(list, 0, 999);
+    linearPrint(list);
+    
+    linearRemoveAtIndex(list, 0);
+    linearPrint(list);
+    
+    linearRemoveValue(list, 10);
+    linearPrint(list);
+    
+    linearRemoveAll(list);
+    linearPrint(list);
+    
+    
+    linearFree(list);
+    
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        test();
     }
     return 0;
 }
+
+
